277abc41624be0851b955c5021bd28ee4424b152
shaliha-sense <shalitha.sense@gmail.com>
2018-05-03 10:55:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shaliha_sense/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.shaliha_sense.myapplication;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      //  TextView textView = (TextView) findViewById(R.id.text_view);
+      //  Typeface font = Typeface.createFromAsset(getAssets(),"iskoolapota.ttf");
+      //  textView.setTypeface(font);
+
+    }
+}
